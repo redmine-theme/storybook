5e12be1e0daead51961b6c3e67a38ba47e023b54
TSURU <tsuyoshi.tsurushima@gmail.com>
2022-10-22 13:57:39 +0900

    ADD storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-viewport"
+  ],
+  "framework": "@storybook/html"
+}
